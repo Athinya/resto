8656cd5f474b8d9e34b68e5daa27a20f244a92cc
Athinya <athinyaa@gmail.com>
2024-12-19 13:24:24 +0530

    v12

--- restos_project/urls.py
+++ restos_project/urls.py
@@ -9,7 +9,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
 
-    1. Add an import:  from others_apps.views import Home
+    1. Add an import:  from others_apps.viewse import Home
 
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
